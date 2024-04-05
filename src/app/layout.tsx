64a473307e73c044a500550e0a064549a8948106
LeeHueeng <zzxx373014@gmail.com>
2024-04-05 20:23:17 +0900

    #2 feat- 메인페이지 정의 및 각 컨테이너 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,43 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
-import Nav from '@/components/nav';
-import '@/app/global.css';
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import Nav from "@/components/navbar";
+import Leftbar from "@/components/leftbar";
+import "@/app/global.css";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: '알법사',
-    description: '알뜰 마법사의 줄인말',
+  title: "알법사",
+  description: "알뜰 마법사의 줄인말",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <Head>
-                <title>{(metadata.title as string) ?? '기본 제목'}</title>
-                <meta name="description" content={metadata.description ?? '기본 설명'} />
-            </Head>
+  return (
+    <html lang="en">
+      <Head>
+        <title>{(metadata.title as string) ?? "기본 제목"}</title>
+        <meta
+          name="description"
+          content={metadata.description ?? "기본 설명"}
+        />
+      </Head>
 
-            <body className={inter.className}>
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: '100vw',
-                        height: '80px',
-                        border: `2px solid #f7f7f7`,
-                    }}
-                >
-                    <Nav />
-                </div>
-                {children}
-            </body>
-        </html>
-    );
+      <body className={inter.className}>
+        <div className="nav-barContainer">
+          <Nav />
+        </div>
+
+        <div className="contents-Container">
+          <div className="contents-Left-Container">
+            <Leftbar />
+          </div>
+          <div className="contents-Right-Container">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
 }
