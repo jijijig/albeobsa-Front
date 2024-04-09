32b745112f1b2a7cc8f0b2b9f4ed023ccec148b2
Hueeng <zzxx373014@gmail.com>
2024-04-09 10:17:19 +0900

    #3 emotion 라이브러리 설치 및 기존 코드 복사 완료

---
+++ src/components/leftbar.tsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { css } from '@emotion/react';
+
+export default function leftbar() {
+    return (
+        <div css={styles}>
+            leftbar
+            <div>dfas</div>
+        </div>
+    );
+}
+
+const styles = css`
+    color: red;
+`;
