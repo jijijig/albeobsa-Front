fbcdde6c2d42455326f77c56c1757f680b1866df
Hueeng <zzxx373014@gmail.com>
2024-04-09 14:19:55 +0900

    #2 각 페이지 생성 및 left bar ui 개선

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Navbar from '@/components/navbar';
+import Leftbar from '@/components/leftbar';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <div className={inter.className}>
+            <div className="nav-barContainer">
+                <Navbar />
+            </div>
+
+            <div className="contents-Container">
+                <div className="contents-Left-Container">
+                    <Leftbar />
+                </div>
+                <div className="contents-Right-Container">
+                    <Component {...pageProps} />
+                </div>
+            </div>
+        </div>
+    );
 }
