2d70d43718e44b29f05a5b2394c0a29296eb44ad
Hueeng <zzxx373014@gmail.com>
2024-04-11 14:09:33 +0900

    #2 feat 커뮤니티 컴포넌트 index 에 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@
 import { css } from "@emotion/react";
 import TopPicks from "@/components/index/TopPicks";
 import Lank from "@/components/index/Lank";
-import Image from "next/image";
 import Community from "@/components/index/community";
 
 export default function Home() {
@@ -14,9 +13,6 @@ export default function Home() {
       <div className="second">
         <Lank />
       </div>
-      <div className="ad">
-        <Image src="/ad.png" width={1023} height={324} alt="상품 이미지" />
-      </div>
       <div className="community">
         <Community />
       </div>
@@ -25,31 +21,16 @@ export default function Home() {
 }
 
 const styles = css`
-  margin 0;
+  margin: 60px;
+  whidth: 100%;
   height: 100%;
-
-  width: 80%;
-  display: flex;
-  flex-direction: column;
-  justify-content: top;
-  align-items: center;
-.topicks{
-  width: 1023px;
-}
+  .topicks {
+    width: 100%;
+  }
   .second {
     margin-top: 37px;
-    width: 1023px;
   }
-  .ad{
-    margin-top: 33px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .community{
-    margin-top: 33px;
-    width: 1023px;
-    height: 550px;
+  .community {
+    margin-top: 37px;
   }
 `;
