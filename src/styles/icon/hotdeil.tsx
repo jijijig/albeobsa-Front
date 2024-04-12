1409f28a4555f7fd5409b10b91aed4954383ce58
Hueeng <zzxx373014@gmail.com>
2024-04-12 15:21:27 +0900

    #2 feat leftbar icon 제작

---
+++ src/styles/icon/hotdeil.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function hotdeil() {
+  return (
+    <svg
+      width="12"
+      height="19"
+      viewBox="0 0 12 19"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M8.03394 18.3264L8.03394 6.33816"
+        stroke="#BCEB00"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.3038 18.3264L11.3038 3.06864"
+        stroke="#BCEB00"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.76489 18.3264L4.76489 9.60769"
+        stroke="#BCEB00"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1.49512 18.3264L1.49512 11.7874"
+        stroke="#BCEB00"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M1 8.24043L8.24043 1"
+        stroke="#BCEB00"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
