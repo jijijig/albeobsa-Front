0b1039297ec0e33450696cf41f8aff90e2b11be9
Hueeng <zzxx373014@gmail.com>
2024-04-30 17:13:48 +0900

    #19 hotdeal 게시판 수정

--- src/components/hotdeal/hotbox.tsx
+++ src/components/hotdeal/hotbox.tsx
@@ -11,9 +11,13 @@ export default function hotbox() {
           <Image src="/ad.png" width={100} height={100} alt="상품" />
         </div>
         <div className="right">
-          <div className="rightleft">
-            <button className="color1">위메프</button>
-            <button className="color2">뽐뿌</button>
+          <div className="rightcontents">
+            <div className="rightleft">
+              <button className="color1">위메프</button>
+              <button className="color2">뽐뿌</button>
+            </div>
+            <div className="title">비비고 왕교자 1.05kg 2팩</div>
+            <div className="price">11,930원(2 x 5,965원)</div>
           </div>
           <div className="rightright"></div>
         </div>
@@ -33,14 +37,37 @@ const style = css`
   gap: 10px;
   .constents {
     display: flex;
-    justify-content: space-between;
+
     align-items: center;
     width: 100%;
     height: 100%;
     gap: 10px;
   }
+  .rightcontents {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 5px;
+    .title {
+      font-family: Poppins;
+      font-size: 13px;
+      font-weight: 400;
+      line-height: 21.24px;
+      text-align: left;
+      color: #858585;
+    }
+    .price {
+      font-family: Poppins;
+      font-size: 13px;
+      font-weight: 400;
+      line-height: 21.24px;
+      text-align: left;
+      color: #f85d5d;
+    }
+  }
   .left {
-    width: 100px;
+    min-width: 100px;
+    width: 10%;
     height: 100px;
     border-radius: 10px;
     border: 2px solid #f0f0f0;
@@ -49,6 +76,7 @@ const style = css`
     width: 10%;
   }
   .right {
+    margin-top: 60px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -66,6 +94,8 @@ const style = css`
       button {
         background-color: #f0f0f0;
         border: none;
+        width: 55px;
+        height: 28px;
         border-radius: 10px;
         padding: 5px;
         margin-right: 10px;
--- src/pages/hotdeals.tsx
+++ src/pages/hotdeals.tsx
@@ -6,12 +6,16 @@ export default function hotdeals() {
   return (
     <div css={style}>
       <Hotbox />
+      <Hotbox /> <Hotbox />
+      <Hotbox /> <Hotbox />
     </div>
   );
 }
 const style = css`
   padding: 20px;
   display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
   justify-content: center;
   width: 100%;
   height: 100%;
